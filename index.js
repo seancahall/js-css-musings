ee1594a664707705210ba45c4ba8dc9f1e02c357
seancahall <sean@greekrush.com>
2017-09-10 20:36:59 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+            // This data variable is a global variable​
+            var data = [
+                {name:"Samantha", age:12},
+                {name:"Alexis", age:14}
+            ];
+            var user = {
+                // local data variable​
+                data    :[
+                    {name:"T. Woods", age:37},
+                    {name:"P. Mickelson", age:43}
+                ],
+                showData:function (event) {
+                    var randomNum = ((Math.random () * 2 | 0) + 1) - 1; 
+                    console.log (this.data[randomNum].name + " " + this.data[randomNum].age);
+                }
+            }
+            // Assign the showData method of the user object to a variable​
+            var showDataVar = user.showData.bind(user);
+            showDataVar(); // Samantha 12 (from the global data array, not from the local data array)​
